53b70223cd20b75f73708fe6a3059182b0864c95
andrew gaitskell <andrew@gaitskell.com>
2024-12-03 15:16:24 +0000

    all containers

--- containers/mariadb/createinitsql.sh
+++
@@ -1,7 +0,0 @@
-echo "CREATE USER '${ENV_MARIADB_USERNAME}'@'localhost' IDENTIFIED BY '${ENV_MARIADB_PASSWORD}';" > /home/home_user/Code/home_podman/mariadb/init.sql
-
-echo "GRANT ALL PRIVILEGES ON *.* TO '${ENV_MARIADB_USERNAME}'@'localhost' WITH GRANT OPTION;" >>  /home/home_user/Code/home_podman/mariadb/init.sql
-
-echo "ALTER USER '${ENV_MARIADB_USERNAME}'@'%' IDENTIFIED BY '${ENV_MARIADB_PASSWORD}';" >>  /home/home_user/Code/home_podman/mariadb/init.sql
-
-echo "GRANT ALL PRIVILEGES ON *.* TO '${ENV_MARIADB_USERNAME}'@'%' WITH GRANT OPTION;" >>  /home/home_user/Code/home_podman/mariadb/init.sql
