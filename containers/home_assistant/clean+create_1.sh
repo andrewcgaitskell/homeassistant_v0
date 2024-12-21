3ce467ac54df35c1ec6172d9f57624dbfd704120
andrewcgaitskell <andrew@gaitskell.com>
2024-12-21 18:30:56 +0000

    Update clean+create_1.sh

--- containers/home_assistant/clean+create_1.sh
+++ containers/home_assistant/clean+create_1.sh
@@ -1,4 +1,9 @@
 podman stop home_assistant_container_1
 podman rm home_assistant_container_1
 
-podman build -f Dockerfile_1 -t home_assistant_image_1 .
+podman build \
+--build-arg=BUILD_ENV_UID=${ENV_UID} \
+--build-arg=BUILD_ENV_USERNAME=${ENV_USERNAME} \
+--build-arg=BUILD_ENV_GID=${ENV_GID} \
+--build-arg=BUILD_ENV_GROUPNAME=${ENV_GROUPNAME} \
+-f Dockerfile_1 -t home_assistant_image_1 .
