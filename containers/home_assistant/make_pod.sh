719315f9ad6b6346038bbabd3508acc88f13d60b
andrewcgaitskell <andrew@gaitskell.com>
2024-12-21 16:40:03 +0000

    Create make_pod.sh

---
+++ containers/home_assistant/make_pod.sh
@@ -0,0 +1,24 @@
+STATUS="home_dev"
+podman pod stop ${STATUS}_pod
+podman pod rm ${STATUS}_pod
+
+uid=${ENV_UID} ##1001
+gid=${ENV_GID} ##1002
+
+subuidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.UIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+subgidSize=$(( $(podman info --format "{{ range \
+   .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
+
+podman pod create \
+--name  ${STATUS}_pod \
+--infra-name infra_dev \
+--network bridge \
+--uidmap 0:1:$uid \
+--uidmap $uid:0:1 \
+--uidmap $(($uid+1)):$(($uid+1)):$(($subuidSize-$uid)) \
+--gidmap 0:1:$gid \
+--gidmap $gid:0:1 \
+--gidmap $(($gid+1)):$(($gid+1)):$(($subgidSize-$gid)) \
+--publish 8123:8123
+
