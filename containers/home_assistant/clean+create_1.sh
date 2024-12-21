033618f9f6e0f1572402acfc5770bbc71e2c26d9
andrewcgaitskell <andrew@gaitskell.com>
2024-12-21 16:50:12 +0000

    Update clean+create_1.sh

--- containers/home_assistant/clean+create_1.sh
+++ containers/home_assistant/clean+create_1.sh
@@ -1,4 +1,4 @@
 podman stop home_assistant_container_1
 podman rm home_assistant_container_1
 
-podman build -t home_assistant_image_1 Dockerfile_1
+podman build -f Dockerfile_1 -t home_assistant_image_1 .
