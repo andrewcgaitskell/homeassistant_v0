389f8aafc6a8a9da22a0cd2f2b2b78becc44771a
andrewcgaitskell <andrew@gaitskell.com>
2024-12-21 16:21:38 +0000

    Update clean+create.sh

--- containers/home_assistant/clean+create.sh
+++ containers/home_assistant/clean+create.sh
@@ -6,6 +6,7 @@ podman build -t home_assistant_image_1 .
 podman run -d \
     --name home_assistant_container_1 \
     --network=host \
-    -e TZ=Your/Timezone \
-    -v /path/to/config:/config \
+    -e TZ=Europe/London \
+    --userns=keep-id \
+    -v /home/pi5ha/homeassistant_v0/containers/home_assistant/config:/config:Z \
     home_assistant_image_1
