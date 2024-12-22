4435c6c9fdc9dafb546a3688c380d4f5cd43dd94
andrewcgaitskell <andrew@gaitskell.com>
2024-12-22 17:07:30 +0000

    Update make_container.sh

--- containers/home_assistant/make_container.sh
+++ containers/home_assistant/make_container.sh
@@ -6,13 +6,13 @@ subuidSize=$(( $(podman info --format "{{ range \
 subgidSize=$(( $(podman info --format "{{ range \
    .Host.IDMappings.GIDMap }}+{{.Size }}{{end }}" ) - 1 ))
 
-mkdir -p /home/pi5ha/homeassistant/config
+mkdir -p /home/pi5ha/homeassistant/test
 
-rm /home/pi5ha/homeassistant/config/configuration.yaml
-cp /home/pi5ha/homeassistant_v0/containers/home_assistant/config/configuration.yaml /home/pi5ha/homeassistant/config/configuration.yaml
+#rm /home/pi5ha/homeassistant/config/configuration.yaml
+#cp /home/pi5ha/homeassistant_v0/containers/home_assistant/config/configuration.yaml /home/pi5ha/homeassistant/config/configuration.yaml
 
-chmod -R g+rwx /home/pi5ha/homeassistant/config
-chmod g+s /home/pi5ha/homeassistant/config
+#chmod -R g+rwx /home/pi5ha/homeassistant/config
+#chmod g+s /home/pi5ha/homeassistant/config
 
 podman stop home_assistant_container_1
 podman rm home_assistant_container_1
@@ -24,6 +24,7 @@ podman run -d \
     --name home_assistant_container_1 \
     -p 8123:8123 \
     -e TZ=Europe/London \
+    -v /home/pi5ha/homeassistant/test:/test \ 
     --user $uid:$gid \
     home_assistant_image_2
 
