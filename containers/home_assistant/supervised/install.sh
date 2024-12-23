4d358dff1c8b3248558414287a472ac051d172a6
andrewcgaitskell <andrew@gaitskell.com>
2024-12-23 14:13:59 +0000

    Create install.sh

---
+++ containers/home_assistant/supervised/install.sh
@@ -0,0 +1,15 @@
+apt install \
+apparmor \
+bluez \
+cifs-utils \
+curl \
+dbus \
+jq \
+libglib2.0-bin \
+lsb-release \
+network-manager \
+nfs-common \
+systemd-journal-remote \
+systemd-resolved \
+udisks2 \
+wget -y
