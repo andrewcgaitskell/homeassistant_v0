5c63d03665df1599098b2865fcb233fb514e9cf4
andrewcgaitskell <andrew@gaitskell.com>
2024-12-23 10:29:57 +0000

    Update make_container.sh

--- containers/home_assistant/make_container.sh
+++ containers/home_assistant/make_container.sh
@@ -8,23 +8,21 @@ subgidSize=$(( $(podman info --format "{{ range \
 
 mkdir -p /home/pi5ha/homeassistant/test
 
-#rm /home/pi5ha/homeassistant/config/configuration.yaml
-#cp /home/pi5ha/homeassistant_v0/containers/home_assistant/config/configuration.yaml /home/pi5ha/homeassistant/config/configuration.yaml
+rm /home/pi5ha/homeassistant/config/configuration.yaml
+cp /home/pi5ha/homeassistant_v0/containers/home_assistant/config/configuration.yaml /home/pi5ha/homeassistant/config/configuration.yaml
 
-#chmod -R g+rwx /home/pi5ha/homeassistant/config
-#chmod g+s /home/pi5ha/homeassistant/config
+chmod -R g+rwx /home/pi5ha/homeassistant/config
+chmod g+s /home/pi5ha/homeassistant/config
 
 podman stop home_assistant_container_1
 podman rm home_assistant_container_1
-
-#podman volume create homeassistant_config_volume
-#podman run -it --rm -v myvolume:/data my-volume-image
 
 podman run -d \
     --name home_assistant_container_1 \
     -p 8123:8123 \
     -e TZ=Europe/London \
     -v /home/pi5ha/homeassistant/test:/test \ 
+    -v /home/pi5ha/homeassistant/config:/config \
     --userns=keep-id \
     home_assistant_image_2
 
