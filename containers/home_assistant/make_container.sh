2c11c0d2186307f7591855b433fdad391443dc73
andrewcgaitskell <andrew@gaitskell.com>
2024-12-23 10:10:13 +0000

    Update make_container.sh

--- containers/home_assistant/make_container.sh
+++ containers/home_assistant/make_container.sh
@@ -25,7 +25,7 @@ podman run -d \
     -p 8123:8123 \
     -e TZ=Europe/London \
     -v /home/pi5ha/homeassistant/test:/test \ 
-    --user $uid:$gid \
+    --userns=keep-id \
     home_assistant_image_2
 
 ## -v /home/pi5ha/homeassistant/config:/config \
