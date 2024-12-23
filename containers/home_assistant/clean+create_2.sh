34125c3359f794d2b5f1eca17b7e56125b9aa9b5
andrewcgaitskell <andrew@gaitskell.com>
2024-12-23 13:55:59 +0000

    Update clean+create_2.sh

--- containers/home_assistant/clean+create_2.sh
+++ containers/home_assistant/clean+create_2.sh
@@ -1,6 +1,7 @@
 podman rm home_assistant_image_2
 
 podman build \
+--format=docker \
 --build-arg=BUILD_ENV_UID=${ENV_UID} \
 --build-arg=BUILD_ENV_USERNAME=${ENV_USERNAME} \
 --build-arg=BUILD_ENV_GID=${ENV_GID} \
