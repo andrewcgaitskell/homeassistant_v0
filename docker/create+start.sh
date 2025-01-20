3b7b2a295ce1217a60f2562fecf9b78a5aebc6a6
andrewcgaitskell <andrew@gaitskell.com>
2025-01-20 17:09:56 +0000

    Create create+start.sh

---
+++ docker/create+start.sh
@@ -0,0 +1,10 @@
+docker run -d \
+  --name homeassistant \
+  --privileged \
+  --restart=unless-stopped \
+  -e TZ=Etc/GMT \
+  -v /home/pi5ha/ha/config:/config \
+  -v /run/dbus:/run/dbus:ro \
+  --network=host \
+  ghcr.io/home-assistant/home-assistant:stable
+  
